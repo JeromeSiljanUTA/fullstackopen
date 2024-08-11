8b6b04f6ca0e7d3df9f6c23ec949f273c8025eae
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-08-11 16:30:28 -0500

    feat(part1/src/App.jsx): Exercise 1.1. complete

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,20 +8,39 @@ const App = () => {
     const exercises3 = 14
     return (
 	<div>
-	    <h1>{course}</h1>
-	    <p>
-		{part1} {exercises1}
-	    </p>
-	    <p>
-		{part2} {exercises2}
-	    </p>
-	    <p>
-		{part3} {exercises3}
-	    </p>
+	    <Header course={course} />
 
-	    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+	    <Content part={part1} exercises={exercises1} />
+	    <Content part={part2} exercises={exercises2} />
+	    <Content part={part3} exercises={exercises3} />
+
+	    <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+
 	</div>
     )
 }
+
+const Header = (props) => {
+    return (
+	<h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+    return (
+	<p>
+	    {props.part} {props.exercises}
+	</p>
+    )
+}
+
+const Total = (props) => {
+    return (
+	<p>
+	    Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+	</p>
+    )
+}
+
 
 export default App
