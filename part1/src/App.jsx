b08724f67c363f04d44c1bc8f9cce13bba91a5ce
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-08-12 18:58:57 -0500

    feat(part1/src/App.jsx): Exercise 1.12 complete

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,66 +1,33 @@
 import { useState } from 'react'
 
 const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
-
+    const anecdotes = [
+	'If it hurts, do it more often.',
+	'Adding manpower to a late software project makes it later!',
+	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	'Premature optimization is the root of all evil.',
+	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+	'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+	'The only way to go fast, is to go well.'
+    ]
+    
+    const [selected, setSelected] = useState(0)
+    
     return (
 	<div>
-	    <h1>give feedback</h1>
-	    <Button text='good' handler={() => setGood(good + 1)}/>
-	    <Button text='neutral' handler={() => setNeutral(neutral + 1)}/>
-	    <Button text='bad' handler={() => setBad(bad + 1)}/>
+	    {anecdotes[selected]}
 
-	    <Statistics good={good} neutral={neutral} bad={bad} />
+	    <br></br>
+
+	    <Button len={anecdotes.length} setSelected={setSelected} />
 	</div>
     )
 }
 
-const Statistics = ({good, neutral, bad}) => {
+const Button = ({len, setSelected}) => {
     return (
-	<div>
-	    <h1>statistics</h1>
-	    <table>
-		<StatisticLine text='good' values={{'good': good, 'neutral': neutral, 'bad': bad}} stat='count'/>
-		<StatisticLine text='neutral' values={{'good': good, 'neutral': neutral, 'bad': bad}} stat='count'/>
-		<StatisticLine text='bad' values={{'good': good, 'neutral': neutral, 'bad': bad}} stat='count'/>
-		<StatisticLine text='all' values={{'good': good, 'neutral': neutral, 'bad': bad}} stat='all'/>
-		<StatisticLine text='pos' values={{'good': good, 'neutral': neutral, 'bad': bad}} stat='pos'/>
-	    </table>
-	</div>
-    )
-}
-
-const StatisticLine = ({text, values, stat}) => {
-    if (stat === 'count'){
-	return (
-	    <tr>
-		<td>{text} {values[text]}</td>
-	    </tr>
-	)
-    }
-    else if (stat === 'all'){
-	return (
-	    <tr>
-		<td>{text} {Object.values(values).reduce((a, b) => a + b, 0)}</td>
-	    </tr>
-	)
-    }
-    else if (stat === 'pos'){
-	return (
-	    <tr>
-		<td>{text} {100 * values['good']/Object.values(values).reduce((a, b) => a + b, 0)}%</td>
-	    </tr>
-	)
-    }
-}
-
-const Button = ({text, handler}) => {
-    return (
-	<button onClick={handler}>
-	    {text}
-	</button>
+	<button onClick={() => setSelected(Math.floor(Math.random() * len))}>next anecdote</button>
     )
 }
 
