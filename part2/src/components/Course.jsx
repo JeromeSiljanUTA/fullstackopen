d4c328c98f8f7a16545dec7c30d3ab1d585f646f
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-08-17 15:47:00 -0500

    feat(part2/): Exercise 2.5 complete

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,12 +1,13 @@
 import Content from "./Content";
 import Header from "./Header";
+import Statistics from "./Statistics";
 
 const Course = ({ course }) => {
-  console.log(course);
   return (
     <div>
       <Header text={course.name} />
       <Content parts={course.parts} />
+      <Statistics parts={course.parts} />
     </div>
   );
 };
