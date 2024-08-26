ba859dcdb9d18affef153d375af20d0aba372277
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-08-26 15:28:38 -0500

    feat(part3/index.js): Exercise 3.6 complete

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -24,18 +25,54 @@ const persons = [
   },
 ];
 
-app.get("/", (request, response) => {
+app.get("/", (_, response) => {
   response.send("<h1>Ollo</h1>");
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", (_, response) => {
   const resp = `<p>Phonebook has info for ${persons.length} people</p>
     <p>${Date()}</p>`;
   response.send(resp);
 });
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
+  if (!request.params.id) {
+    response.json(persons);
+  } else {
+    console.log(persons);
+    response.json(persons.find((person) => person.id == request.params.id));
+  }
+});
+
+app.get("/api/persons/", (_, response) => {
   response.json(persons);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  persons = persons.filter((person) => person.id !== request.params.id);
+  response.status(204).end();
+});
+
+app.post("/api/persons/", (request, response) => {
+  if (!request.body.name) {
+    response.status(400).json({ error: "missing name" });
+  } else if (!request.body.number) {
+    response.status(400).json({ error: "missing number" });
+  } else if (persons.find((person) => person.name === request.body.name)) {
+    response.status(400).json({ error: "name already in phonebook" });
+  } else {
+    persons = [
+      ...persons,
+      {
+        id: Math.floor(Math.random() * 10000),
+        name: request.body.name,
+        number: request.body.number,
+      },
+    ];
+
+    console.log(persons);
+    response.status(200).end();
+  }
 });
 
 const PORT = 3001;
