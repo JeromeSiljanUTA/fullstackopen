571c05b0d26ae8f6c74ddb66afa6034d7fe0a27b
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-12-21 14:03:40 -0600

    feat(part3/index.js): Completed exercise 3.13 and 3.14

--- part3/index.js
+++ part3/index.js
@@ -5,6 +5,8 @@ import { Person } from "./models/persons.js";
 
 const app = express();
 app.use(express.json());
+
+// serve up dist directory on /
 app.use(express.static("dist"));
 
 morgan.token("body", (request, _) => {
@@ -16,26 +18,17 @@ app.use(cors());
 
 const url = process.env.MONGODB_URI;
 
-let persons = [];
-
-app.get("/", (_, response) => {
-  response.send("<h1>Ollo</h1>");
-});
-
 app.get("/info", (_, response) => {
-  const resp = `<p>Phonebook has info for ${persons.length} people</p>
+  Person.countDocuments().then((count) => {
+    const resp = `<p>Phonebook has info for ${count} people</p>
     <p>${Date()}</p>`;
-  response.send(resp);
+    response.send(resp);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  if (!request.params.id) {
-    Person.find().then((persons) => {
-      response.json(persons);
-    });
-  } else {
-    response.json(persons.find((person) => person.id == request.params.id));
-  }
+  const query = Person.where({ _id: request.params.id });
+  query.findOne().then((person) => response.json(person));
 });
 
 app.get("/api/persons/", (_, response) => {
@@ -45,30 +38,21 @@ app.get("/api/persons/", (_, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  console.log("please remove person with id", request.params.id);
   const query = Person.where({ _id: request.params.id });
-  query.findOneAndDelete().then((persons) => response.status(204).end());
+  query.findOneAndDelete().then((person) => response.status(204).end());
 });
 
 app.post("/api/persons/", (request, response) => {
-  if (!request.body.name) {
-    response.status(400).json({ error: "missing name" });
-  } else if (!request.body.number) {
-    response.status(400).json({ error: "missing number" });
-  } else if (persons.find((person) => person.name === request.body.name)) {
-    response.status(400).json({ error: "name already in phonebook" });
-  } else {
-    // persons = [
-    //   ...persons,
-    //   {
-    //     id: Math.floor(Math.random() * 10000).toString(),
-    //     name: request.body.name,
-    //     number: request.body.number,
-    //   },
-    // ];
-
-    response.status(200).end();
-  }
+  Person.create({
+    name: request.body.name,
+    number: request.body.number,
+  })
+    .then((mongoResponse) => {
+      response.status(200).end();
+    })
+    .catch((err) => {
+      response.status(400).json({ error: err.message });
+    });
 });
 
 const PORT = process.env.PORT || 3001;
