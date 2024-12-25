40ebc24dde29f4212e8223a4f60aa0ca095b2821
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-12-24 18:14:39 -0600

    feat(part4): Completed 4.1, 4.2

--- part4/app.js
+++ part4/app.js
@@ -2,16 +2,8 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
 const mongoose = require("mongoose");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -19,22 +11,6 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(blogsRouter);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
